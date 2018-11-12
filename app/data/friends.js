8a512a2db357abba0eb7d5e955fea5e24b8fc209
Callie Hart <callie_miller@hotmail.com>
2018-11-11 19:43:08 -0600

    updated files

---
+++ app/data/friends.js
@@ -0,0 +1,100 @@
+const friends = [
+    {
+        "name":"Rachel Green",
+        "photo":"https://en.wikipedia.org/wiki/File:Jennifer_Aniston_as_Rachel_Green.jpg",
+        "scores":[
+            2,
+            3,
+            4,
+            2,
+            3,
+            3,
+            4,
+            4,
+            4,
+            3
+          ]
+      },
+      {
+        "name":"Monica Geller",
+        "photo":"https://en.wikipedia.org/wiki/File:Courteney_Cox_as_Monica_Geller.jpg",
+        "scores":[
+            3,
+            4,
+            5,
+            4,
+            3,
+            5,
+            5,
+            4,
+            5,
+            4
+          ]
+      },
+      {
+        "name":"Phoebe Buffay",
+        "photo":"https://en.wikipedia.org/wiki/File:Friendsphoebe.jpg",
+        "scores":[
+            5,
+            2,
+            3,
+            1,
+            2,
+            4,
+            1,
+            2,
+            4,
+            5
+          ]
+      },
+      {
+        "name":"Joey Tribbiani",
+        "photo":"https://en.wikipedia.org/wiki/File:Matt_LeBlanc_as_Joey_Tribbiani.jpg",
+        "scores":[
+            5,
+            1,
+            1,
+            1,
+            2,
+            1,
+            3,
+            1,
+            1,
+            4
+          ]
+      },
+      {
+        "name":"Chandler Bing",
+        "photo":"https://en.wikipedia.org/wiki/File:Matthew_Perry_as_Chandler_Bing.jpg",
+        "scores":[
+            2,
+            4,
+            3,
+            3,
+            4,
+            3,
+            2,
+            3,
+            4,
+            3
+          ]
+      },
+      {
+        "name":"Ross Geller",
+        "photo":"https://en.wikipedia.org/wiki/File:David_Schwimmer_as_Ross_Geller.jpg",
+        "scores":[
+            4,
+            5,
+            2,
+            5,
+            4,
+            3,
+            1,
+            5,
+            5,
+            2
+          ]
+      }
+];
+
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,12 @@
+const friendsData = require("../data/friends");
+
+module.exports = function(app) {
+    app.get("/api/friends", function(req, res){
+        res.json(friends);
+    });
+
+    app.post("/api/friends", function(req, res){
+        friendsData.push(req.body);
+        res.json(true);
+    });
+};
